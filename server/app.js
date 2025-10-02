1302ebed5b016bf35127c3cdc2e7cb83eb0ac5c2
miramsalp <Thanapat_petch@hotmail.com>
2025-10-02 22:06:10 +0700

    feat: add auth controller, route

--- server/app.js
+++ server/app.js
@@ -2,11 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+const authRoutes = require('./routes/authRoutes');
+
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.status(200).json({ message: "Welcome to Krapong-Go API!" });
 });
+
+app.use('/api/auth', authRoutes);
 
 module.exports = app;
