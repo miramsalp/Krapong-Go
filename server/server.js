d93951963475e99c2a5f1746641fd4e16224d01d
miramsalp <Thanapat_petch@hotmail.com>
2025-10-02 22:34:27 +0700

    feat: add socket

--- server/server.js
+++ server/server.js
@@ -2,12 +2,25 @@ const http = require("http");
 const dotenv = require('dotenv');
 const app = require("./app.js");
 const connectDB = require('./config/db')
+const { Server } = require("socket.io");
+const initializeSocket = require('./config/socketHandler.js'); 
 
 dotenv.config({ path: './.env' });
 
 connectDB();
 
 const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*", 
+        methods: ["GET", "POST"]
+    }
+});
+
+app.set('socketio', io); 
+initializeSocket(io);
+
 const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
