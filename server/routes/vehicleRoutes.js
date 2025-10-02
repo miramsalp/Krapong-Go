78d6f5da800400e625e76e75a77c0abb7adfbe14
miramsalp <Thanapat_petch@hotmail.com>
2025-10-02 22:25:55 +0700

    feat: vehicle positon to controller

--- server/routes/vehicleRoutes.js
+++ server/routes/vehicleRoutes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const vehicleController = require('../controllers/vehicleController');
 const { protect, restrictTo } = require('../middlewares/authMiddleware');
 
-router.post('/', protect, restrictTo('driver'), vehicleController.registerVehicle);
+router.route('/').post(protect, restrictTo('driver'), vehicleController.registerVehicle).get(vehicleController.getActiveVehiclesOnRoute); 
+
+router.patch('/my-vehicle/location', protect, restrictTo('driver'), vehicleController.updateMyVehicleLocation);
+router.patch('/my-vehicle/status', protect, restrictTo('driver'), vehicleController.updateMyVehicleStatus);
 
 module.exports = router;
