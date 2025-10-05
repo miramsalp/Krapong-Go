9d509c9def97f940cd03f46f9d5ed3cae1a34443
miramsalp <Thanapat_petch@hotmail.com>
2025-10-05 14:55:51 +0700

    design: fix passenger menu design

--- client/src/components/PassengerMenu.jsx
+++ client/src/components/PassengerMenu.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import './PassengerMenu.css';
 
 function PassengerMenu({ hasActivePing, onPing, onCancelPing }) {
     return (
-        <div>
+        <div className="passenger-menu-container"> 
             <h3>Passenger Menu</h3>
             {hasActivePing ? (
-                <button onClick={onCancelPing} style={{ background: 'orange' }}>ยกเลิกการเรียกรถ</button>
+                <button onClick={onCancelPing} className="action-button cancel">ยกเลิกการเรียกรถ</button>
             ) : (
-                <button onClick={onPing}>ปักหมุดเรียกรถ</button>
+                <button onClick={onPing} className="action-button primary">ปักหมุดเรียกรถ</button>
             )}
         </div>
     );
